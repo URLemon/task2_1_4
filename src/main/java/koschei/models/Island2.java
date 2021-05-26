9245ca0381b2db77a3bb1f1dae1ef3863794f7e2
URLemon <ksavdoninmail@gmail.com>
2021-05-26 18:07:22 +0300

    complete task

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,10 +1,12 @@
 package koschei.models;
 
-public class Island2 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+public class Island2 {
     private Wood3 wood;
 
-    public Island2(Wood3 wood) {
+    public Island2(@Autowired Wood3 wood) {
         this.wood = wood;
     }
 
